3f4f1e554787b9a3c6a73175dfc1e27225eb5b1c
Daniels Bunka <daniels.bunka8@gmail.com>
2026-02-25 02:25:15 +0000

    Added a function to send messages without prompting

--- textapp.py
+++ textapp.py
@@ -1,20 +1,48 @@
+import os
+from dotenv import load_dotenv
+from twilio.rest import Client
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 
+# Load up all the variables from .env file
+load_dotenv()
+TWILIO_SID = os.getenv("TWILIO_SID")
+TWILIO_TOKEN = os.getenv("TWILIO_TOKEN")
+TWILIO_PHONE = os.getenv("TWILIO_PHONE")
+PERSONAL_PHONE = os.getenv("PERSONAL_PHONE")
+TRAIN_API = os.getenv("TRAIN_API")
+
+#----------
+# Unprompted Send Function
+#----------
+def instant_send(textbody):
+      client = Client(TWILIO_SID, TWILIO_TOKEN)
+      
+      message = client.messages.create(
+            body=textbody,
+            from_=TWILIO_PHONE,
+            to=PERSONAL_PHONE
+      )
+
+      print("Unprompted message sent! \n Message ID: " + message.sid )
+
+#----------
+# Webhook Listening Part
+#----------
 # Starts the Flask web framework
 app = Flask(__name__)
 
-#This opens a webpage that ends with with /sms
+# This opens a webpage that ends with with /sms
 # Tells it to expect data - a POST request
+# app.route connects it to the listen_reply function
 @app.route("/sms", methods=['POST'])
-
 def listen_reply():
     
-    # Reads the request that Twilo sent
+    # Reads the request that Twilio sent
     incoming_request = request.values.get('Body','').lower().strip()
     print("Message arrived " + incoming_request)
 
-    # Needed to be able to send a request back to Twilo
+    # Needed to be able to send a request back to Twilio
     resp = MessagingResponse()
 
     if incoming_request == '.ping':
@@ -26,5 +54,10 @@ def listen_reply():
 
     return str(resp)
 
+
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000)
+
+    
